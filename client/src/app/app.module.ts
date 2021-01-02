62dbb836d927a8be48680370e8e3ea35fb8d10a9
Keith Tharp <keithstharp@gmail.com>
2021-01-02 10:09:08 -0500

    Made my first component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AuthModule } from "@auth0/auth0-angular";
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { HttpClientModule } from '@angular/common/http';
+import { KeithTestComponent } from './components/keith-test/keith-test/keith-test.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { HttpClientModule } from '@angular/common/http';
     HomeComponent,
     LoginComponent,
     ProfileComponent,
+    KeithTestComponent,
 
   ],
   imports: [
---
+++ client/src/app/components/keith-test/keith-test/keith-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-keith-test',
+  templateUrl: './keith-test.component.html',
+  styleUrls: ['./keith-test.component.scss']
+})
+export class KeithTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
